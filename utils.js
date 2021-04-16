f4b368a0fc2958d823e2a6ae1bf9cd6debbe70f2
mikhailevseev <mevseev@zendesk.com>
2021-04-16 18:12:45 +0200

    Added authentication

--- utils.js
+++ utils.js
@@ -7,4 +7,14 @@ function createSubject() {
     return new rxjs.BehaviorSubject(DEFAULT_FRAME)
 }
 
-module.exports = { createSubject, DEFAULT_FRAME }
+
+function authenticated(authHeaderString) {
+    if (!authHeaderString) {
+        return false
+    }
+    const [login, password] = new Buffer(authHeaderString.split(' ')[1], 'base64').toString().split(':')
+
+    return login === process.env.AUTH_LOGIN && password === process.env.AUTH_PASSWORD
+}
+
+module.exports = { createSubject, authenticated, DEFAULT_FRAME }
